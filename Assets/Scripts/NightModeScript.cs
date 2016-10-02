03282242c1ae1862cdebef7cf92b7d6d63df7be4
l0fty-WhizZ <shreyas@shitjustgotreal.in>
2016-10-02 16:32:00 +0530

    Added a script which changes the material of the entities when the night mode kicks in.

---
+++ Assets/Scripts/NightModeScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NightModeScript : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject playerPrefab;
+
+	[SerializeField]
+	private GameObject AIPrefab;
+
+	[SerializeField]
+	private Camera mainCam;
+
+	[SerializeField]
+	private Material nightModeMat;
+
+	[SerializeField]
+	private Material origSnakeMat;
+
+	[SerializeField]
+	private Material origAIMat;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject gameController = GameObject.Find ("GameController");
+		GameController gc = gameController.GetComponent<GameController> ();
+
+		if (gc.score >= 10) {
+			playerPrefab.GetComponent<Renderer> ().sharedMaterial = nightModeMat;
+			AIPrefab.GetComponent<Renderer> ().sharedMaterial = nightModeMat;
+			mainCam.backgroundColor = new Color (0.0f, 0.0f, 0.0f);
+		} else {
+			playerPrefab.GetComponent<Renderer> ().sharedMaterial = origSnakeMat;
+			AIPrefab.GetComponent<Renderer> ().sharedMaterial = origAIMat;
+		}
+	}
+		
+}
